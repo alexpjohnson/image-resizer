87c5af7a8e7707bd3d2e30b6821a52ee107dae3e
Alex Johnson <ajohnson@nearinfinity.com>
2013-08-16 18:22:05 -0400

    Updated readme. Got download and display working

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 image-resizer
 =============
+
+Web service that takes a url, height, and width parameter to resize an image.
+
+Requires ImageMagick to be installed prior to installation.
--- resizer.rb
+++ resizer.rb
@@ -1,13 +1,21 @@
 require 'sinatra'
 require 'mini_magick'
 
-get '/resizer' do 
-	url = params[:url]
-	puts url
-	puts params[:height]
-	puts params[:width]
-	image = MiniMagick::Image.open(url)
-	image.resize "#{params[:height]}x#{params[:width]}"
-	image.write "resized-image.png"
-		
+get '/resizer/display' do 
+	image = MiniMagick::Image.open(params[:url])
+	if (params[:height] && params[:width])
+		image.resize "#{params[:height]}x#{params[:width]}"
+		send_file image.path, :type => image.mime_type
+	end
+end
+
+get '/resizer/download' do
+	image = MiniMagick::Image.open(params[:url])
+	if (params[:height] && params[:width])
+		image.resize "#{params[:height]}x#{params[:width]}"
+		f = File.new(image.path)
+		response.headers['content-type'] = "application/octet-stream"
+		attachment("resized-image.#{image.mime_type.split('/')[1]}")
+		response.write(f)
+	end
 end

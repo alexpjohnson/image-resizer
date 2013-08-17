7b85bfd4f9e6e290718c020740d234996919441e
Alex Johnson <ajohnson@nearinfinity.com>
2013-08-17 01:03:01 -0400

    Added error handling for non-supported file types

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 image-resizer
 =============
 
-Web service that takes a url, height, and width parameter to resize an image.
+Web service that takes a url, height, and width parameter to resize an image. Supported file types are: bmp, png, jpeg, jpg, tiff, png, tga
 
 Requires ImageMagick to be installed prior to installation.
 
@@ -11,3 +11,8 @@ To download the resized image
 
 To view the resized image in the browser
 \<host url\>/resizer/display?url=\<image url\>&height=\<new height\>&width=\<new width\>
+
+Live @ Heroku
+You can view this application at http://ajohnson-image-resizer.herokuapp.com/
+
+Example: http://ajohnson-image-resizer.herokuapp.com/?url=http://i.imgur.com/KLxTM.jpg&height=500&width=500
--- app/resizer.rb
+++ app/resizer.rb
@@ -10,20 +10,32 @@ get '/*' do
 end
 
 get %r{/(download|display)} do
+	file_formats = ['bmp', 'png', 'jpeg', 'jpg', 'tiff', 'png', 'tga']
 	if /display/.match(request.path_info)
 		image = MiniMagick::Image.open(params[:url])
-		if (params[:height] && params[:width])
-			image.resize "#{params[:height]}x#{params[:width]}"
-			send_file image.path, :type => image.mime_type
+		if file_formats.include? image.mime_type.split('/')[1]
+			if (params[:height] && params[:width])
+				image.resize "#{params[:height]}x#{params[:width]}"
+				send_file image.path, :type => image.mime_type
+				send_file File.join('public', 'index.html')
+			end
+		else
+			send_file File.join('public', 'error.html')
 		end
 	elsif /download/.match(request.path_info)
 		image = MiniMagick::Image.open(params[:url])
-		if (params[:height] && params[:width])
-			image.resize "#{params[:height]}x#{params[:width]}"
-			f = File.new(image.path)
-			response.headers['content-type'] = "application/octet-stream"
-			attachment("resized-image.#{Time.now.utc.to_i}.#{image.mime_type.split('/')[1]}")
-			response.write(f)
+		if file_formats.include? image.mime_type.split('/')[1]
+			if (params[:height] && params[:width])
+				image.resize "#{params[:height]}x#{params[:width]}"
+				f = File.new(image.path)
+				response.headers['content-type'] = "application/octet-stream"
+				attachment("resized-image.#{Time.now.utc.to_i}.#{image.mime_type.split('/')[1]}")
+				response.write(f)
+				send_file File.join('public', 'index.html')
+			else
+				send_file File.join('public', 'error.html')
+			end
 		end
 	end
 end
+
